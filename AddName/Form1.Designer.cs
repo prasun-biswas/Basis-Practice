d7262174a1e146f28212b96de18091b5254a499f
prasun-biswas <rpblikescodes>
2014-09-29 16:47:55 +0600

    add file

---
+++ AddName/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AddName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EmployeetextBox = new System.Windows.Forms.TextBox();
+            this.countLevel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.addNameTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.EmployeetextBox);
+            this.groupBox1.Controls.Add(this.countLevel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.addNameTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(54, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(336, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Employee:";
+            // 
+            // EmployeetextBox
+            // 
+            this.EmployeetextBox.Location = new System.Drawing.Point(97, 14);
+            this.EmployeetextBox.Name = "EmployeetextBox";
+            this.EmployeetextBox.Size = new System.Drawing.Size(112, 20);
+            this.EmployeetextBox.TabIndex = 4;
+            // 
+            // countLevel
+            // 
+            this.countLevel.AutoSize = true;
+            this.countLevel.Location = new System.Drawing.Point(124, 76);
+            this.countLevel.Name = "countLevel";
+            this.countLevel.Size = new System.Drawing.Size(0, 13);
+            this.countLevel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Count";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(239, 40);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // addNameTextBox
+            // 
+            this.addNameTextBox.Location = new System.Drawing.Point(49, 40);
+            this.addNameTextBox.Name = "addNameTextBox";
+            this.addNameTextBox.Size = new System.Drawing.Size(160, 20);
+            this.addNameTextBox.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(54, 151);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 121);
+            this.listBox1.TabIndex = 1;
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(315, 151);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 4;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(315, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 309);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "EmployeeList";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label countLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox addNameTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EmployeetextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AddName/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using  System.Collections;
+
+namespace AddName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            addButton.Enabled = false;
+        }
+
+    List<string> name=new List<string>();
+        private int Count = 0;
+        private int Countlimit = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+          Countlimit = Convert.ToInt32(EmployeetextBox.Text);
+          addButton.Enabled = true;
+            button1.Enabled = false;
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+       // name[Count] = addNameTextBox.Text;
+            name.Add(addNameTextBox.Text);
+            label1.Text = (++Count).ToString();
+        
+            if (Count == Countlimit)
+            {
+               
+                addButton.Enabled = false;
+            }
+            
+           
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < Count; i++)
+            {
+                listBox1.Items.Add(name[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            addButton.Enabled = true;
+            label1.Text = "";
+            addNameTextBox.Text = "";
+            Count = 1;
+            EmployeetextBox.Text = "";
+            button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+    }
+}
+
+
+
+
+
+
+
+
